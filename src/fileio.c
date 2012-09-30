a608f0bf126e306634714188ee7eacfd8bd52e6a
Johnny on Flame <johnnyonflame@hotmail.com>
2012-09-30 12:40:29 -0300

    Small clean-up and fixes.

--- src/fileio.c
+++ src/fileio.c
@@ -253,7 +253,7 @@ int loadMap(char *fileName)
 			}
 		}
 		if(!strcmp(words[0], "ENTITY:")) {
-			if (n < 2) {
+			if (n < 3) {
 				printf("Malformed entity, no %s specified.\n", (n==1) ? "object name" : "arguments");
 				fclose(ifp);
 				return 1;
